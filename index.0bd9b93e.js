cfe6a56c19bd6925a49c3a7c4944d7a8e68c18a2
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-26 19:18:55 +0000

    Deploying to gh-pages from @ matoka02/goit-js-hw-10@af6cb405e52852610341dd02d33ef3c6d87626e7 🚀

---
+++ index.0bd9b93e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".breed-select");document.querySelector(".loader"),document.querySelector(".error");e.addEventListener("change",(function(e){console.log(e.currentTarget),e.preventDefault()}))}();
+//# sourceMappingURL=index.0bd9b93e.js.map
---
+++ index.1620aeec.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".breed-select");document.querySelector(".loader"),document.querySelector(".error");e.addEventListener("change",(function(e){console.log(e.currentTarget),e.preventDefault()}));
+//# sourceMappingURL=index.1620aeec.js.map
