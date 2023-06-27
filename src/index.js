78dc5e613819818b2a288652eb1a1827d6feee2f
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-27 10:48:15 +0300

    task

--- src/index.js
+++ src/index.js
@@ -11,41 +11,67 @@ const LIST_URL = 'https://api.thecatapi.com/v1/breeds/';
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
 const API_KEY = 'live_Nyc5epOZ5A7aIisug3oaSTpi5tyQSrOU7MxmwOH7IUlCWiCTMoHx2WLptYl1EpUG';
 
-select.addEventListener('change', onSearch);
-
-function onSearch(evt) {
-    console.log(evt.currentTarget);
-    evt.preventDefault();
-    
-
-    // const
-}
-
-
-
-
-
-
-
-
-
-
-
+// select.addEventListener('change', onSearch);
 
 
 // коллекция пород
 function fetchBreeds() {
     return fetch(`${LIST_URL}`)
     .then(resp => {
-        console.log(resp);
+        console.log(resp)
         if(!resp.ok){
             throw new Error(resp.statusText)
         }
         // return []
-        return resp.json()
+        // return resp.json()
     })
     .catch(err => console.error(err))
 }
+fetchBreeds()
+
+
+
+function onSearchBreeds(evt) {
+    evt.preventDefault()
+
+    const {
+        id: {
+            value: idBreeds
+        },
+        name: {
+            value: nameBreeds
+        }
+    } = evt.currentTarget.elements;
+
+    if (!idBreeds) {
+        alert('Поле пусте 😢');
+        return;
+    }
+
+    fetchBreeds(idBreeds, nameBreeds).then(data => {
+        console.log(data);
+        createMarkup(data)
+        });
+}
+
+function createMarkup(arr) {
+    const markup = arr.map(item => `<option value="${item.idBreeds}">${item.nameBreeds}</option>`).join('');
+
+    select.addEventListener()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // идентификатор
 // fetchCatByBreed(breedId)
