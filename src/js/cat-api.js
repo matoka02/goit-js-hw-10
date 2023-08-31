49a05eec67dfe2c725a1eeabbb2f37ae6f8ea4d7
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-31 20:16:46 +0300

    fixed

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,42 +1,39 @@
-"use strict";
+'use strict';
 
 const LIST_URL = 'https://api.thecatapi.com/v1/breeds/';
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
-const API_KEY = 'live_mKiv7hRKzdNDTibRgNQFYq36GalsTTZaPuEdSRPUO447TaWFbtFTO1YFYZyDQGTz';
-
+const API_KEY =
+  'live_mKiv7hRKzdNDTibRgNQFYq36GalsTTZaPuEdSRPUO447TaWFbtFTO1YFYZyDQGTz';
 
 // Коллекция пород
 
 function fetchBreeds() {
-    return fetch(`${LIST_URL}`)
+  return fetch(`${LIST_URL}`)
     .then(resp => {
-        // console.log(resp)       // приходит ответ на запрос пород
-        if(!resp.ok){
-            if (response.status === 404) {
-                return [];
-                }
-            throw new Error(resp.statusText);
+      // console.log(resp)       // приходит ответ на запрос пород
+      if (!resp.ok) {
+        if (response.status === 404) {
+          return [];
         }
-        return resp.json();
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
     })
-    .catch(err => console.error(err))
-};
+    .catch(err => console.error(err));
+}
 
 // Информация о коте
 
 function fetchCatByBreed(breedId) {
-    return fetch(`${BASE_URL}?breed_ids=${breedId}&api_key=${API_KEY}`)
+  return fetch(`${BASE_URL}?breed_ids=${breedId}&api_key=${API_KEY}`)
     .then(resp => {
-        // console.log(resp);      // приходит ответ на запрос
-        if(!resp.ok){
-            throw new Error(resp.statusText)
-        }
-        return resp.json()
+      // console.log(resp);      // приходит ответ на запрос
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
     })
-    .catch(err => console.error(err))
-};
+    .catch(err => console.error(err));
+}
 
-
-
-
-export {fetchBreeds, fetchCatByBreed};
+export { fetchBreeds, fetchCatByBreed };
